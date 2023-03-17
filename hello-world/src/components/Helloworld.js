1c679b7de44db8e042223214970d29eaa4e046aa
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-03-17 22:51:23 +0300

    Add hello world text

---
+++ hello-world/src/components/Helloworld.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assests/Helloworld.css";
+
+function Helloworld() {
+  return (
+    <div className="hello-div">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Helloworld;
